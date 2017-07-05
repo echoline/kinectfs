a05cbc00f207d48b0b587d39770368c6c46f0387
eli <eli@robot>
2017-07-05 21:48:28 +0000

    Qedge, Qbw...

--- kinectfs/kinectfs.c
+++ kinectfs/kinectfs.c
@@ -9,26 +9,29 @@
 #include <jpeglib.h>
 #endif
 
-unsigned short d2rlut[2048];
+unsigned char r2blut[0x1000000];
+unsigned char k2rlut[0x800][3];
 enum {
 	Qroot = 0,
 	Qrgb,
 	Qdepth,
 	Qtilt,
 	Qled,
+	Qextra,
+	Qedge,
+	Qbw,
 #ifdef USE_AUDIO
 	Qmic0,
 	Qmic1,
 	Qmic2,
 	Qmic3,
 #endif
-	Qextra,
 };
-char *paths[] = { "/", "rgb", "depth", "tilt", "led",
+char *paths[] = { "/", "rgb", "depth", "tilt",
+	"led", "extra", "edge", "bw",
 #ifdef USE_AUDIO
 	"mic0", "mic1", "mic2", "mic3",
 #endif
-	"extra",
 	 };
 int Npaths = (sizeof(paths)/sizeof(paths[0]));
 unsigned long mtimes[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -37,10 +40,16 @@ unsigned long atimes[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 typedef struct {
 	unsigned char* image;
 	unsigned long length;
+	unsigned int width;
+	unsigned int height;
+	unsigned int components;
+	unsigned int colorspace;
 } FrnctImg;
 static FrnctImg rgbimg;
 static FrnctImg depthimg;
 static FrnctImg extraimg;
+static FrnctImg edgeimg;
+static FrnctImg bwimg;
 static struct timeval rgbdlast;
 static int rgbdlock = 0;
 static freenect_raw_tilt_state *tiltstate = NULL;
@@ -81,9 +90,12 @@ newfidaux(int path) {
 	FidAux *ret;
 
 	ret = calloc (1, sizeof(FidAux));
-	if (path == Qrgb || path == Qdepth || path == Qextra) {
+	if (path == Qrgb || path == Qdepth || path == Qextra || path == Qedge || path == Qbw) {
 		ret->fim = calloc(1, sizeof(FrnctImg));
-		ret->fim->length = 640*480*3+HDRLEN;
+		if (path != Qbw)
+			ret->fim->length = 640*480*3+HDRLEN;
+		else
+			ret->fim->length = 640*480*1+HDRLEN;
 		ret->fim->image = calloc(1, ret->fim->length);
 	}
 
@@ -204,10 +216,10 @@ compressjpg(FrnctImg *img)
 	cinfo.err = jpeg_std_error( &jerr );
 	jpeg_create_compress(&cinfo);
 
-	cinfo.image_width = 640;
-	cinfo.image_height = 480;
-	cinfo.input_components = 3;
-	cinfo.in_color_space = JCS_RGB;
+	cinfo.image_width = img->width;
+	cinfo.image_height = img->height;
+	cinfo.input_components = img->components;
+	cinfo.in_color_space = img->colorspace;
 
 	jpeg_mem_dest( &cinfo, &buf, &buflen );
 	jpeg_set_defaults( &cinfo );
@@ -221,7 +233,7 @@ compressjpg(FrnctImg *img)
 
 	jpeg_finish_compress( &cinfo );
 
-	if (buflen < (640*480*3+HDRLEN))
+	if (buflen < img->length)
 		img->length = buflen;
 	memcpy(img->image, buf, img->length);
 
@@ -238,7 +250,7 @@ static void fs_open(Ixp9Req *r)
 	unsigned char *depthbuf;
 	int c;
 	unsigned short s;
-	int j, k, l, m;
+	int j, k, l, x, xm, xp, y, ym, yp;
 	int path = r->fid->qid.path;
 
 	if (path < 0 || path >= Npaths) {
@@ -251,7 +263,7 @@ static void fs_open(Ixp9Req *r)
 	r->fid->aux = newfidaux(path);
 	f = r->fid->aux;
 
-	if (path == Qrgb || path == Qdepth || path == Qextra) {
+	if (path == Qrgb || path == Qdepth || path == Qextra || path == Qedge || path == Qbw) {
 		r->ofcall.ropen.iounit = 680*480*4;
 		if (istime(&rgbdlast, 1.0/30.0)) {
 			freenect_sync_get_tilt_state(&tiltstate, 0);
@@ -266,71 +278,84 @@ static void fs_open(Ixp9Req *r)
 			}
 			rgbdlock = 1;
 
-			rgbimg.length = 640*480*3+HDRLEN;
-			depthimg.length = 640*480*3+HDRLEN;
-			extraimg.length = 640*480*3+HDRLEN;
+			rgbimg.width = 640;
+			rgbimg.height = 480;
+			rgbimg.components = 3;
+			rgbimg.colorspace = JCS_RGB;
+			rgbimg.length = rgbimg.width*rgbimg.height*rgbimg.components+HDRLEN;
 			memcpy(rgbimg.image, "P6\n640 480\n255\n", HDRLEN);
+
+			depthimg.width = 640;
+			depthimg.height = 480;
+			depthimg.components = 3;
+			depthimg.colorspace = JCS_RGB;
+			depthimg.length = depthimg.width*depthimg.height*depthimg.components+HDRLEN;
 			memcpy(depthimg.image, "P6\n640 480\n255\n", HDRLEN);
+
+			extraimg.width = 640;
+			extraimg.height = 480;
+			extraimg.components = 3;
+			extraimg.colorspace = JCS_RGB;
+			extraimg.length = extraimg.width*extraimg.height*extraimg.components+HDRLEN;
 			memcpy(extraimg.image, "P6\n640 480\n255\n", HDRLEN);
 
+			edgeimg.width = 640;
+			edgeimg.height = 480;
+			edgeimg.components = 3;
+			edgeimg.colorspace = JCS_RGB;
+			edgeimg.length = edgeimg.width*edgeimg.height*edgeimg.components+HDRLEN;
+			memcpy(edgeimg.image, "P6\n640 480\n255\n", HDRLEN);
+
+			bwimg.width = rgbimg.width;
+			bwimg.height = rgbimg.height;
+			bwimg.components = 1;
+			bwimg.colorspace = JCS_GRAYSCALE;
+			bwimg.length = bwimg.width*bwimg.height*bwimg.components+HDRLEN;
+			memcpy(rgbimg.image, "P5\n640 480\n255\n", HDRLEN);
+
 			memcpy(rgbimg.image+HDRLEN, rgbbuf, rgbimg.length-HDRLEN);
+			memset(edgeimg.image+HDRLEN, 0, edgeimg.length-HDRLEN);
+			// need this first for edges
 			for (j = 0; j < (640 * 480); j++) {
+				c = r2blut[((*(uint32_t*)&rgbbuf[j * 3]) & 0xFFFFFF00) >> 8];
+				bwimg.image[j + HDRLEN] = (c & 0xFF);
+			}
+			for (x = 0; x < 640; x++) for (y = 0; y < 480; y++) {
+				j = y * 640 + x;
+				// TODO only works when HDRLEN is multiple of 3
+				l = j + (HDRLEN/3);
+
+				if (x > 0 && x < 639 && y > 0 && y < 479) {
+					k = abs(bwimg.image[(j - 641) + HDRLEN] * -1 + bwimg.image[(j - 639) + HDRLEN] * 1 + bwimg.image[(j - 1) + HDRLEN] * -2 + bwimg.image[(j + 1) + HDRLEN] * 2 + bwimg.image[(j + 639) + HDRLEN] * -1 + bwimg.image[(j + 641) + HDRLEN] * 1 + bwimg.image[(j - 641) + HDRLEN] * 1 + bwimg.image[(j - 640) + HDRLEN] * 2 + bwimg.image[(j - 639) + HDRLEN] * 1 + bwimg.image[(j + 639) + HDRLEN] * -1 + bwimg.image[(j + 640) + HDRLEN] * -2 + bwimg.image[(j + 641) + HDRLEN] * -1);
+					if (k >= 140)
+						memset(&edgeimg.image[l * 3], k & 0xFF, 3);
+				}
+
 				s = depthbuf[j * 2];
 				s |= depthbuf[j * 2 + 1] << 8;
 	
 				if (s >= 2048)
 					s = 0;
 
-				k = d2rlut[s];
-				c = k & 0xff;
-				// TODO only works when HDRLEN is multiple of 3
-				l = j + (HDRLEN/3);
+				k = s & 0x7FF;
+				memcpy(&depthimg.image[l * 3], k2rlut[k], 3);
 
-				switch (k >> 8) {
-				case 0:
-					depthimg.image[l * 3] = 255;
-					depthimg.image[l * 3 + 1] = 255 - c;
-					depthimg.image[l * 3 + 2] = 255 - c;
-					break;
-				case 1:
-					depthimg.image[l * 3] = 255;
-					depthimg.image[l * 3 + 1] = c;
-					depthimg.image[l * 3 + 2] = 0;
-					break;
-				case 2:
-					depthimg.image[l * 3] = 255 - c;
-					depthimg.image[l * 3 + 1] = 255;
-					depthimg.image[l * 3 + 2] = 0;
-					break;
-				case 3:
-					depthimg.image[l * 3] = 0;
-					depthimg.image[l * 3 + 1] = 255;
-					depthimg.image[l * 3 + 2] = c;
-					break;
-				case 4:
-					depthimg.image[l * 3] = 0;
-					depthimg.image[l * 3 + 1] = 255 - c;
-					depthimg.image[l * 3 + 2] = 255;
-					break;
-				case 5:
-					depthimg.image[l * 3] = 0;
-					depthimg.image[l * 3 + 1] = 0;
-					depthimg.image[l * 3 + 2] = 255 - c;
-					break;
-				default:
-					depthimg.image[l * 3] = 0;
-					depthimg.image[l * 3 + 1] = 0;
-					depthimg.image[l * 3 + 2] = 0;
-					break;
+				if (memcmp(&depthimg.image[l * 3], "\x00\x00\x00", 3) != 0)
+					for (k = 0; k < 3; k++)
+						extraimg.image[l * 3 + k] = (depthimg.image[l * 3 + k] * 0.3) + (bwimg.image[j + HDRLEN] * 0.7);
+				else
+					memset(&extraimg.image[l * 3], bwimg.image[j + HDRLEN], 3);
+				if (edgeimg.image[l * 3] != 0) {
+					extraimg.image[l * 3] = edgeimg.image[l * 3];
+					extraimg.image[l * 3 + 1] = extraimg.image[l * 3 + 2] = 0;
 				}
-				extraimg.image[l * 3] = round(depthimg.image[l * 3] * 0.4 + rgbimg.image[l * 3] * 0.6);
-				extraimg.image[l * 3 + 1] = round(depthimg.image[l * 3 + 1] * 0.4 + rgbimg.image[l * 3 + 1] * 0.6);
-				extraimg.image[l * 3 + 2] = round(depthimg.image[l * 3 + 2] * 0.4 + rgbimg.image[l * 3 + 2] * 0.6);
 			}
 #ifdef USE_JPEG
 			compressjpg(&rgbimg);
 			compressjpg(&depthimg);
 			compressjpg(&extraimg);
+			compressjpg(&edgeimg);
+			compressjpg(&bwimg);
 #endif
 			rgbdlock = 0;
 		}
@@ -350,6 +375,14 @@ RGBDLOCK:
 		case Qextra:
 			memcpy(f->fim->image, extraimg.image, extraimg.length);
 			f->fim->length = extraimg.length;
+			break;
+		case Qedge:
+			memcpy(f->fim->image, edgeimg.image, edgeimg.length);
+			f->fim->length = edgeimg.length;
+			break;
+		case Qbw:
+			memcpy(f->fim->image, bwimg.image, bwimg.length);
+			f->fim->length = bwimg.length;
 			break;
 		default:
 			respond(r, "file not found");
@@ -457,6 +490,8 @@ static void fs_read(Ixp9Req *r)
 	case Qrgb:
 	case Qdepth:
 	case Qextra:
+	case Qedge:
+	case Qbw:
 		size = r->ifcall.tread.count;
 		if (size < 0) {
 			respond(r, "no.");
@@ -744,7 +779,7 @@ freenect_do_one (long ms, void *aux)
 
 int
 main(int argc, char *argv[]) {
-	int i, fd;
+	int i, fd, c;
 	IxpConn *acceptor;
 	freenect_context *f_ctx;
 	float v;
@@ -770,10 +805,41 @@ main(int argc, char *argv[]) {
 
 	freenect_sync_set_led (led, 0);
 
-	for (i = 0; i < 2048; i++) {
-		v = i/2048.0;
-		v = powf(v, 3) * 6;
-		d2rlut[i] = v * 6 * 256;
+	for (i = 0; i < 0x1000000; i++) {
+		r2blut[i] = (double)((i & 0xFF0000) >> 16) * 0.2989 +
+			    (double)((i & 0xFF00) >> 8) * 0.5870 +
+			    (double)(i & 0xFF) * 0.1140;
+	}
+	for (i = 0; i < 0x800; i++) {
+		c = ((i & 0x1FF)/(double)0x200)*256;
+		switch (i & 0x600) {
+		case 0x000:
+			if (c == 0)
+				memset(k2rlut[i], 0x00, 3);
+			else {
+				k2rlut[i][0] = 255;
+				k2rlut[i][1] = c;
+				k2rlut[i][2] = 0;
+			}
+			break;
+		case 0x200:
+			k2rlut[i][0] = 255 - c;
+			k2rlut[i][1] = 255;
+			k2rlut[i][2] = 0;
+			break;
+		case 0x400:
+			k2rlut[i][0] = 0;
+			k2rlut[i][1] = 255;
+			k2rlut[i][2] = c;
+			break;
+		case 0x600:
+			k2rlut[i][0] = 0;
+			k2rlut[i][1] = 255 - c;
+			k2rlut[i][2] = 255;
+			break;
+		default:
+			debug ("k: %x\n", k);
+		}
 	}
 
 #ifdef USE_AUDIO
@@ -791,9 +857,13 @@ main(int argc, char *argv[]) {
 	rgbimg.length = 640*480*3+HDRLEN;
 	depthimg.length = 640*480*3+HDRLEN;
 	extraimg.length = 640*480*3+HDRLEN;
+	edgeimg.length = 640*480*3+HDRLEN;
+	bwimg.length = 640*480*1+HDRLEN;
 	rgbimg.image = calloc(1, rgbimg.length);
 	depthimg.image = calloc(1, depthimg.length);
 	extraimg.image = calloc(1, extraimg.length);
+	edgeimg.image = calloc(1, edgeimg.length);
+	bwimg.image = calloc(1, bwimg.length);
 
 	fd = ixp_announce (argv[1]);
 	if (fd < 0) {
